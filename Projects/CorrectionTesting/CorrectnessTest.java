67bb8f4fa2273f273087bbd60e4fd7d67a1a4ee7
Joseph Ledesma <46974518+JGLedesma@users.noreply.github.com>
2021-03-10 16:30:42 -0700

    Add files via upload

---
+++ Projects/CorrectionTesting/CorrectnessTest.java
@@ -0,0 +1,66 @@
+include package parspice;
+include package jnispice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java;
+
+// Get a batch INTO the code to work with, then test a function with an incoming batch, etc
+
+public class CorrectnessTest {
+
+    @Test
+    // Test error conditions matrix, improper data types
+    void mxmTest(ArrayList mxmArray) {
+        try {
+            if ()
+        } catch (Exception e) {
+            System.out.println("arrayTest Error");
+        }
+    }
+
+    @Test
+    void str2etTest(ArrayList str2etArray) {
+        try {
+            if () {
+
+            }
+        } catch (Exception e) {
+            System.out.println("arrayTest Error");
+        }
+    }
+
+    @Test
+    void spkezrTest(ArrayList spkezrArray) {
+        try {
+            if () {
+
+            }
+        } catch (Exception e) {
+            System.out.println("arrayTest Error");
+        }
+    }
+
+    @Test
+    void singleTest(Object objOne, Object objTwo) {
+        try {
+            if (objOne isInstanceOf String) {
+
+            }
+
+            if (objOne isInstanceOf Double) {
+
+            }
+
+            if (objOne isInstanceOf Integer) {
+
+            }
+        } catch (Exception e) {
+            System.out.println("singleTest Error");
+        }
+    }
+}
